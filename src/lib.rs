4f865ac4f92d16a7fe87b460677b7a12942fab5b
Dumble009 <daichi19990821@gmail.com>
2024-10-13 21:47:27 +0900

    三角形を着色するように変更

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use rand::thread_rng;
+use rand::Rng;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use web_sys::console;
@@ -10,11 +12,30 @@ use web_sys::console;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+fn draw_triangle(
+    context: &web_sys::CanvasRenderingContext2d,
+    points: [(f64, f64); 3],
+    color: (u8, u8, u8),
+) {
+    context.move_to(points[0].0, points[0].1); // top of triangle
+    context.begin_path();
+    context.line_to(points[1].0, points[1].1); // bottom left of triangle
+    context.line_to(points[2].0, points[2].1); // bottom right of triangle
+    context.line_to(points[0].0, points[0].1); // back to top of triangle
+    context.close_path();
+    context.stroke();
+    let color_str = format!("rgb({}, {}, {})", color.0, color.1, color.2);
+    context.set_fill_style_str(&color_str);
+    context.fill();
+}
+
 fn draw_sierpinski(
     context: &web_sys::CanvasRenderingContext2d,
     points: [(f64, f64); 3],
+    color: (u8, u8, u8),
     depth: u32,
 ) {
+    draw_triangle(context, points, color);
     if depth > 0 {
         let top_left_center = (
             (points[0].0 + points[1].0) / 2.0,
@@ -28,29 +49,33 @@ fn draw_sierpinski(
             (points[1].0 + points[2].0) / 2.0,
             (points[1].1 + points[2].1) / 2.0,
         );
+
+        let mut rng = thread_rng();
+        let next_color = (
+            rng.gen_range(0..255),
+            rng.gen_range(0..255),
+            rng.gen_range(0..255),
+        );
         draw_sierpinski(
             context,
             [points[0], top_left_center, top_right_center],
+            next_color,
             depth - 1,
         );
         draw_sierpinski(
             context,
             [top_left_center, points[1], left_right_center],
+            next_color,
             depth - 1,
         );
         draw_sierpinski(
             context,
             [top_right_center, left_right_center, points[2]],
+            next_color,
             depth - 1,
         );
     } else {
-        context.move_to(points[0].0, points[0].1); // top of triangle
-        context.begin_path();
-        context.line_to(points[1].0, points[1].1); // bottom left of triangle
-        context.line_to(points[2].0, points[2].1); // bottom right of triangle
-        context.line_to(points[0].0, points[0].1); // back to top of triangle
-        context.close_path();
-        context.stroke();
+        draw_triangle(context, points, color);
     }
 }
 
@@ -74,6 +99,11 @@ pub fn main_js() -> Result<(), JsValue> {
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
 
-    draw_sierpinski(&context, [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)], 5);
+    draw_sierpinski(
+        &context,
+        [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)],
+        (0, 255, 0),
+        5,
+    );
     Ok(())
 }
