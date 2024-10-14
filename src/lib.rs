819ddcfe83776b55fe544234e8d317254bd3b601
Dumble009 <daichi19990821@gmail.com>
2024-10-14 11:50:02 +0900

    アニメーションするようにした

--- src/lib.rs
+++ src/lib.rs
@@ -130,33 +130,6 @@ pub fn main_js() -> Result<(), JsValue> {
 
     wasm_bindgen_futures::spawn_local(async move {
         let image = web_sys::HtmlImageElement::new().unwrap();
-        let (success_tx, success_rx) = futures::channel::oneshot::channel::<Result<(), JsValue>>();
-        let success_tx = Rc::new(Mutex::new(Some(success_tx)));
-        let error_tx = Rc::clone(&success_tx);
-        let callback = Closure::once(move || {
-            if let Some(success_tx) = success_tx.lock().ok().and_then(|mut opt| opt.take()) {
-                success_tx.send(Ok(()));
-            }
-            web_sys::console::log_1(&JsValue::from_str("loaded!"));
-        });
-        let error_callback = Closure::once(move |err| {
-            if let Some(error_tx) = error_tx.lock().ok().and_then(|mut opt| opt.take()) {
-                error_tx.send(Err(err));
-            }
-        });
-        image.set_onload(Some(callback.as_ref().unchecked_ref()));
-        image.set_onerror(Some(error_callback.as_ref().unchecked_ref()));
-        image.set_src("Idle (1).png");
-        success_rx.await;
-        context.draw_image_with_html_image_element(&image, 0.0, 0.0);
-
-        draw_sierpinski(
-            &context,
-            [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)],
-            (0, 255, 0),
-            5,
-        );
-
         let json = fetch_json("rhb.json")
             .await
             .expect("Could not featch rhb.json");
@@ -181,18 +154,30 @@ pub fn main_js() -> Result<(), JsValue> {
         image.set_onerror(Some(error_callback.as_ref().unchecked_ref()));
         image.set_src("rhb.png");
         success_rx.await;
-        let sprite = sheet.frames.get("Run (1).png").expect("Cell not found");
-        context.draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
-            &image,
-            sprite.frame.x.into(),
-            sprite.frame.y.into(),
-            sprite.frame.w.into(),
-            sprite.frame.h.into(),
-            300.0,
-            300.0,
-            sprite.frame.w.into(),
-            sprite.frame.h.into(),
+
+        let mut frame = -1;
+        let interval_callback = Closure::wrap(Box::new(move || {
+            frame = (frame + 1) % 8;
+            let frame_name = format!("Run ({}).png", frame + 1);
+            let sprite = sheet.frames.get(&frame_name).expect("Cell not found");
+            context.clear_rect(0.0, 0.0, 600.0, 600.0);
+            context.draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
+                &image,
+                sprite.frame.x.into(),
+                sprite.frame.y.into(),
+                sprite.frame.w.into(),
+                sprite.frame.h.into(),
+                300.0,
+                300.0,
+                sprite.frame.w.into(),
+                sprite.frame.h.into(),
+            );
+        }) as Box<dyn FnMut()>);
+        window.set_interval_with_callback_and_timeout_and_arguments_0(
+            interval_callback.as_ref().unchecked_ref(),
+            50,
         );
+        interval_callback.forget();
     });
     web_sys::console::log_1(&JsValue::from_str("waiting..."));
     Ok(())
