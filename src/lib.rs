a2ebf9396ccd850030b9abf8505eca374803fedd
Dumble009 <daichi19990821@gmail.com>
2024-10-19 14:39:37 +0900

    ブラウザが絡む処理を browser.rs にまとめ、lib.rsをリファクタ

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,9 @@ use std::sync::Mutex;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 
+#[macro_use]
+mod browser;
+
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
 //
@@ -100,37 +103,15 @@ fn draw_sierpinski(
     }
 }
 
-async fn fetch_json(json_path: &str) -> Result<JsValue, JsValue> {
-    let window = web_sys::window().unwrap();
-    let resp_value = wasm_bindgen_futures::JsFuture::from(window.fetch_with_str(json_path)).await?;
-    let resp: web_sys::Response = resp_value.dyn_into()?;
-
-    wasm_bindgen_futures::JsFuture::from(resp.json()?).await
-}
-
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
     console_error_panic_hook::set_once();
+    let context = browser::context().expect("Could not get browser context");
 
-    let window = web_sys::window().unwrap();
-    let document = window.document().unwrap();
-    let canvas = document
-        .get_element_by_id("canvas")
-        .unwrap()
-        .dyn_into::<web_sys::HtmlCanvasElement>()
-        .unwrap();
-
-    let context = canvas
-        .get_context("2d")
-        .unwrap()
-        .unwrap()
-        .dyn_into::<web_sys::CanvasRenderingContext2d>()
-        .unwrap();
-
-    wasm_bindgen_futures::spawn_local(async move {
+    browser::spawn_local(async move {
         let image = web_sys::HtmlImageElement::new().unwrap();
-        let json = fetch_json("rhb.json")
+        let json = browser::fetch_json("rhb.json")
             .await
             .expect("Could not featch rhb.json");
         let sheet: Sheet = serde_wasm_bindgen::from_value(json)
@@ -161,22 +142,25 @@ pub fn main_js() -> Result<(), JsValue> {
             let frame_name = format!("Run ({}).png", frame + 1);
             let sprite = sheet.frames.get(&frame_name).expect("Cell not found");
             context.clear_rect(0.0, 0.0, 600.0, 600.0);
-            context.draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
-                &image,
-                sprite.frame.x.into(),
-                sprite.frame.y.into(),
-                sprite.frame.w.into(),
-                sprite.frame.h.into(),
-                300.0,
-                300.0,
-                sprite.frame.w.into(),
-                sprite.frame.h.into(),
-            );
+            let _ = context
+                .draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
+                    &image,
+                    sprite.frame.x.into(),
+                    sprite.frame.y.into(),
+                    sprite.frame.w.into(),
+                    sprite.frame.h.into(),
+                    300.0,
+                    300.0,
+                    sprite.frame.w.into(),
+                    sprite.frame.h.into(),
+                );
         }) as Box<dyn FnMut()>);
-        window.set_interval_with_callback_and_timeout_and_arguments_0(
-            interval_callback.as_ref().unchecked_ref(),
-            50,
-        );
+        let _ = browser::window()
+            .unwrap()
+            .set_interval_with_callback_and_timeout_and_arguments_0(
+                interval_callback.as_ref().unchecked_ref(),
+                50,
+            );
         interval_callback.forget();
     });
     web_sys::console::log_1(&JsValue::from_str("waiting..."));
