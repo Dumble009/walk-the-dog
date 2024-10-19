cb2a63506114ead21a0b54b78af3bdaf839ca4c8
Dumble009 <daichi19990821@gmail.com>
2024-10-19 15:18:12 +0900

    engineモジュールを実装し、画像読み込み処理をそこに移譲した

--- src/browser.rs
+++ src/browser.rs
@@ -1,8 +1,10 @@
 use anyhow::{anyhow, Result};
 use std::future::Future;
+use wasm_bindgen::closure::WasmClosureFnOnce;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use wasm_bindgen_futures::JsFuture;
+use web_sys::HtmlImageElement;
 use web_sys::{CanvasRenderingContext2d, Document, HtmlCanvasElement, Response, Window};
 
 macro_rules! log{
@@ -68,4 +70,15 @@ pub async fn fetch_json(json_path: &str) -> Result<JsValue> {
     )
     .await
     .map_err(|err| anyhow!("error fetching JSON {:#?}", err))
+}
+
+pub fn new_image() -> Result<HtmlImageElement> {
+    HtmlImageElement::new().map_err(|err| anyhow!("Could not create HtmlImageElement: {:#?}", err))
+}
+
+pub fn closure_once<F, A, R>(fn_once: F) -> Closure<F::FnMut>
+where
+    F: 'static + WasmClosureFnOnce<A, R>,
+{
+    Closure::once(fn_once)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ use wasm_bindgen::JsCast;
 
 #[macro_use]
 mod browser;
+mod engine;
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
@@ -110,31 +111,15 @@ pub fn main_js() -> Result<(), JsValue> {
     let context = browser::context().expect("Could not get browser context");
 
     browser::spawn_local(async move {
-        let image = web_sys::HtmlImageElement::new().unwrap();
         let json = browser::fetch_json("rhb.json")
             .await
             .expect("Could not featch rhb.json");
         let sheet: Sheet = serde_wasm_bindgen::from_value(json)
             .expect("Could not convert rhb.json into a Sheet structure.");
 
-        let (success_tx, success_rx) = futures::channel::oneshot::channel::<Result<(), JsValue>>();
-        let success_tx = Rc::new(Mutex::new(Some(success_tx)));
-        let error_tx = Rc::clone(&success_tx);
-        let callback = Closure::once(move || {
-            if let Some(success_tx) = success_tx.lock().ok().and_then(|mut opt| opt.take()) {
-                success_tx.send(Ok(()));
-            }
-            web_sys::console::log_1(&JsValue::from_str("loaded!"));
-        });
-        let error_callback = Closure::once(move |err| {
-            if let Some(error_tx) = error_tx.lock().ok().and_then(|mut opt| opt.take()) {
-                error_tx.send(Err(err));
-            }
-        });
-        image.set_onload(Some(callback.as_ref().unchecked_ref()));
-        image.set_onerror(Some(error_callback.as_ref().unchecked_ref()));
-        image.set_src("rhb.png");
-        success_rx.await;
+        let image = engine::load_image("rhb.png")
+            .await
+            .expect("Could not load rhb.png");
 
         let mut frame = -1;
         let interval_callback = Closure::wrap(Box::new(move || {
