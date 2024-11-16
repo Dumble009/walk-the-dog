481da42f95ce5e92e0d9cba24c7129599d8393c5
Dumble009 <daichi19990821@gmail.com>
2024-11-16 11:43:15 +0900

    RedHatBoy 構造体を使用してキャラクターを描画するように変更

--- src/game.rs
+++ src/game.rs
@@ -61,6 +61,36 @@ impl RedHatBoy {
             image: image,
         }
     }
+
+    fn draw(&self, renderer: &Renderer) {
+        let frame_name = format!(
+            "{} ({}).png",
+            self.state_machine.frame_name(),
+            (self.state_machine.context().frame / 3) + 1
+        );
+
+        let sprite = self
+            .sprite_sheet
+            .frames
+            .get(&frame_name)
+            .expect("Cell not found");
+
+        renderer.draw_image(
+            &self.image,
+            &Rect {
+                x: sprite.frame.x.into(),
+                y: sprite.frame.y.into(),
+                width: sprite.frame.w.into(),
+                height: sprite.frame.h.into(),
+            },
+            &Rect {
+                x: self.state_machine.context().position.x.into(),
+                y: self.state_machine.context().position.y.into(),
+                width: sprite.frame.w.into(),
+                height: sprite.frame.h.into(),
+            },
+        );
+    }
 }
 
 #[derive(Copy, Clone)]
@@ -73,6 +103,8 @@ mod red_hat_boy_states {
     use super::RedHatBoyStateMachine;
     use crate::engine::Point;
     const FLOOR: i16 = 475;
+    const IDLE_FRAME_NAME: &str = "Idle";
+    const RUN_FRAME_NAME: &str = "Run";
 
     #[derive(Copy, Clone)]
     pub struct RedHatBoyState<S> {
@@ -80,11 +112,17 @@ mod red_hat_boy_states {
         _state: S,
     }
 
+    impl<S> RedHatBoyState<S> {
+        pub fn context(&self) -> &RedHatBoyContext {
+            &self.context
+        }
+    }
+
     #[derive(Copy, Clone)]
     pub struct RedHatBoyContext {
-        frame: u8,
-        position: Point,
-        velocity: Point,
+        pub frame: u8,
+        pub position: Point,
+        pub velocity: Point,
     }
 
     #[derive(Copy, Clone)]
@@ -111,6 +149,16 @@ mod red_hat_boy_states {
                 _state: Idle {},
             }
         }
+
+        pub fn frame_name(&self) -> &str {
+            IDLE_FRAME_NAME
+        }
+    }
+
+    impl RedHatBoyState<Running> {
+        pub fn frame_name(&self) -> &str {
+            RUN_FRAME_NAME
+        }
     }
 
     impl From<RedHatBoyState<Running>> for RedHatBoyStateMachine {
@@ -128,6 +176,20 @@ mod red_hat_boy_states {
             match (self, event) {
                 (RedHatBoyStateMachine::Idle(state), Event::Run) => state.run().into(),
                 _ => self,
+            }
+        }
+
+        pub fn frame_name(&self) -> &str {
+            match self {
+                RedHatBoyStateMachine::Idle(state) => state.frame_name(),
+                RedHatBoyStateMachine::Running(state) => state.frame_name(),
+            }
+        }
+
+        pub fn context(&self) -> &RedHatBoyContext {
+            match self {
+                RedHatBoyStateMachine::Idle(state) => &state.context(),
+                RedHatBoyStateMachine::Running(state) => &state.context(),
             }
         }
     }
@@ -207,5 +269,7 @@ impl Game for WalkTheDog {
                 },
             );
         });
+
+        self.rhb.as_ref().unwrap().draw(renderer);
     }
 } // impl Game for WalkTheDog
