695db230f1dac5be8c13aebbe5ef08afff235a31
Dumble009 <daichi19990821@gmail.com>
2024-11-30 11:18:03 +0900

    RedHatBoy 構造体を実装する以前の描画や更新処理を削除

--- src/game.rs
+++ src/game.rs
@@ -28,22 +28,12 @@ pub struct Sheet {
 }
 
 pub struct WalkTheDog {
-    image: Option<HtmlImageElement>,
-    sheet: Option<Sheet>,
-    frame: u8,
-    position: Point,
     rhb: Option<RedHatBoy>,
 }
 
 impl WalkTheDog {
     pub fn new() -> Self {
-        WalkTheDog {
-            image: None,
-            sheet: None,
-            frame: 0,
-            position: Point { x: 0, y: 0 },
-            rhb: None,
-        }
+        WalkTheDog { rhb: None }
     }
 }
 
@@ -258,10 +248,6 @@ impl Game for WalkTheDog {
             .expect("Could not convert rhb.json into a Sheet structure.");
         let image = Some(engine::load_image("rhb.png").await?);
         Ok(Box::new(WalkTheDog {
-            image: image.clone(),
-            sheet: sheet.clone(),
-            frame: 0,
-            position: self.position,
             rhb: Some(RedHatBoy::new(
                 sheet.clone().ok_or_else(|| anyhow!("No Sheet Present"))?,
                 image.clone().ok_or_else(|| anyhow!("No Imgage Present"))?,
@@ -270,8 +256,6 @@ impl Game for WalkTheDog {
     }
 
     fn update(&mut self, keystate: &KeyState) {
-        self.frame = (self.frame + 1) % 23;
-
         let mut velocity = Point { x: 0, y: 0 };
         if keystate.is_pressed("ArrowDown") {
             velocity.y += 3;
@@ -290,42 +274,15 @@ impl Game for WalkTheDog {
             velocity.x -= 3;
         }
 
-        self.position.x += velocity.x;
-        self.position.y += velocity.y;
-
         self.rhb.as_mut().unwrap().update();
     }
 
     fn draw(&self, renderer: &Renderer) {
-        let frame_name = format!("Run ({}).png", (self.frame / 3) + 1);
-        let sprite = self
-            .sheet
-            .as_ref()
-            .and_then(|sheet| sheet.frames.get(&frame_name))
-            .expect("Cell not found");
         renderer.clear(&Rect {
             x: 0.0,
             y: 0.0,
             width: 600.0,
             height: 600.0,
-        });
-
-        self.image.as_ref().map(|image| {
-            renderer.draw_image(
-                &image,
-                &Rect {
-                    x: sprite.frame.x.into(),
-                    y: sprite.frame.y.into(),
-                    width: sprite.frame.w.into(),
-                    height: sprite.frame.h.into(),
-                },
-                &Rect {
-                    x: self.position.x.into(),
-                    y: self.position.y.into(),
-                    width: sprite.frame.w.into(),
-                    height: sprite.frame.h.into(),
-                },
-            );
         });
 
         self.rhb.as_ref().unwrap().draw(renderer);
