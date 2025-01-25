006cb350d642cf111044222c8fb798fe6a235b8a
Dumble009 <daichi19990821@gmail.com>
2025-01-25 14:22:51 +0900

    ゲームオーバーになったらやり直せるようにした

--- src/browser.rs
+++ src/browser.rs
@@ -5,7 +5,9 @@ use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use wasm_bindgen_futures::JsFuture;
 use web_sys::js_sys::ArrayBuffer;
-use web_sys::{CanvasRenderingContext2d, Document, HtmlCanvasElement, Response, Window};
+use web_sys::{
+    CanvasRenderingContext2d, Document, HtmlCanvasElement, HtmlElement, Response, Window,
+};
 use web_sys::{Element, HtmlImageElement};
 
 macro_rules! log{
@@ -145,7 +147,25 @@ pub fn hide_ui() -> Result<()> {
         ui.remove_child(&child)
             .map(|_removed_child| ())
             .map_err(|err| anyhow!("Failed to remove child {:#?}", err))
+            .and_then(|_unit| {
+                canvas()?
+                    .focus()
+                    .map_err(|err| anyhow!("Could not set focus to canvas! {:#?}", err))
+            })
     } else {
         Ok(())
     }
+}
+
+pub fn find_html_element_by_id(id: &str) -> Result<HtmlElement> {
+    document()
+        .and_then(|doc| {
+            doc.get_element_by_id(id)
+                .ok_or_else(|| anyhow!("Element with id {} not found", id))
+        })
+        .and_then(|element| {
+            element
+                .dyn_into::<HtmlElement>()
+                .map_err(|err| anyhow!("Could not cast into HtmlElement {:#?}", err))
+        })
 }
--- src/game.rs
+++ src/game.rs
@@ -9,6 +9,7 @@ use crate::engine::{Cell, Game, Image, Point, Rect, Renderer, Sheet};
 use crate::segment::{platform_and_stone, stone_and_platform, Disturbee, Obstacle};
 use anyhow::{anyhow, Result};
 use async_trait::async_trait;
+use futures::channel::mpsc::UnboundedReceiver;
 use rand::{thread_rng, Rng};
 use std::collections::btree_map::Keys;
 use std::rc::Rc;
@@ -67,12 +68,32 @@ impl Walk {
     fn knocked_out(&self) -> bool {
         self.boy.knocked_out()
     }
+
+    fn reset(walk: Self) -> Self {
+        let starting_obstacles =
+            stone_and_platform(walk.stone.clone(), walk.obstacle_sheet.clone(), 0);
+        let timeline = rightmost(&starting_obstacles);
+
+        Walk {
+            boy: RedHatBoy::reset(walk.boy),
+            backgrounds: walk.backgrounds,
+            obstacles: starting_obstacles,
+            obstacle_sheet: walk.obstacle_sheet,
+            stone: walk.stone,
+            timeline,
+        }
+    }
 }
 
 pub struct WalkTheDog {
     machine: Option<WalkTheDogStateMachine>,
 }
 
+impl WalkTheDog {
+    pub fn new() -> Self {
+        WalkTheDog { machine: None }
+    }
+}
 enum WalkTheDogStateMachine {
     Ready(WalkTheDogState<Ready>),
     Walking(WalkTheDogState<Walking>),
@@ -86,11 +107,13 @@ struct WalkTheDogState<T> {
 
 struct Ready;
 struct Walking;
-struct GameOver;
+struct GameOver {
+    new_game_event: UnboundedReceiver<()>,
+}
 
-impl WalkTheDog {
-    pub fn new() -> Self {
-        WalkTheDog { machine: None }
+impl GameOver {
+    fn new_game_pressed(&mut self) -> bool {
+        matches!(self.new_game_event.try_next(), Ok(Some(())))
     }
 }
 
@@ -188,6 +211,15 @@ impl RedHatBoy {
 
     fn knocked_out(&self) -> bool {
         self.state_machine.knocked_out()
+    }
+
+    fn reset(boy: Self) -> Self {
+        RedHatBoy::new(
+            boy.sprite_sheet,
+            boy.image,
+            boy.state_machine.context().audio.clone(),
+            boy.state_machine.context().jump_sound.clone(),
+        )
     }
 }
 
@@ -394,8 +426,8 @@ mod red_hat_boy_states {
         pub frame: u8,
         pub position: Point,
         pub velocity: Point,
-        audio: Audio,
-        jump_sound: Sound,
+        pub audio: Audio,
+        pub jump_sound: Sound,
     }
 
     #[derive(Copy, Clone)]
@@ -929,8 +961,14 @@ impl WalkTheDogState<Walking> {
     }
 
     fn end_game(self) -> WalkTheDogState<GameOver> {
+        let receiver = browser::draw_ui("<button id='new_game'>New Game</button>")
+            .and_then(|_unit| browser::find_html_element_by_id("new_game"))
+            .map(|element| engine::add_click_handler(element))
+            .unwrap();
         WalkTheDogState {
-            _state: GameOver,
+            _state: GameOver {
+                new_game_event: receiver,
+            },
             walk: self.walk,
         }
     }
@@ -951,8 +989,34 @@ impl From<WalkingEndState> for WalkTheDogStateMachine {
 }
 
 impl WalkTheDogState<GameOver> {
-    fn update(self) -> WalkTheDogState<GameOver> {
-        self
+    fn update(mut self) -> GameOverEndState {
+        if self._state.new_game_pressed() {
+            GameOverEndState::Complete(self.new_game())
+        } else {
+            GameOverEndState::Continue(self)
+        }
+    }
+
+    fn new_game(self) -> WalkTheDogState<Ready> {
+        browser::hide_ui();
+        WalkTheDogState {
+            _state: Ready,
+            walk: Walk::reset(self.walk),
+        }
+    }
+}
+
+enum GameOverEndState {
+    Complete(WalkTheDogState<Ready>),
+    Continue(WalkTheDogState<GameOver>),
+}
+
+impl From<GameOverEndState> for WalkTheDogStateMachine {
+    fn from(state: GameOverEndState) -> Self {
+        match state {
+            GameOverEndState::Complete(ready) => ready.into(),
+            GameOverEndState::Continue(game_over) => game_over.into(),
+        }
     }
 }
 
